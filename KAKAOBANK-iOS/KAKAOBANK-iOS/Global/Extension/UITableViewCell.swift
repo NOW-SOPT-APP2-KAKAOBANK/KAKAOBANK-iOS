da6ba66c70a572e9651efa8a8573f881c7364bae
seuriseuljjeok <heeseul09@naver.com>
2024-05-13 03:55:03 +0900

    [Add] UITabelViewCell extension 추가 (#3)

--- KAKAOBANK-iOS/KAKAOBANK-iOS/Global/Extension/UITableViewCell.swift
+++ KAKAOBANK-iOS/KAKAOBANK-iOS/Global/Extension/UITableViewCell.swift
@@ -5,4 +5,13 @@
 //  Created by 윤희슬 on 5/13/24.
 //
 
-import Foundation
+import UIKit
+
+extension UITableViewCell {
+    
+    // Cell identifier
+    static var cellIdentifier : String {
+        return String(describing: self)
+    }
+    
+}
