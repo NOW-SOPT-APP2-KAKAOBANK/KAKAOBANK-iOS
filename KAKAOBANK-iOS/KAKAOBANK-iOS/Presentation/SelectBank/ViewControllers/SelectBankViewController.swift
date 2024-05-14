5795b273b520a22f8b2be849a70bf977f3aab4f9
seuriseuljjeok <heeseul09@naver.com>
2024-05-15 05:21:50 +0900

    [Feat] 세그먼트 구현 (#18)

--- KAKAOBANK-iOS/KAKAOBANK-iOS/Presentation/SelectBank/ViewControllers/SelectBankViewController.swift
+++ KAKAOBANK-iOS/KAKAOBANK-iOS/Presentation/SelectBank/ViewControllers/SelectBankViewController.swift
@@ -7,23 +7,74 @@
 
 import UIKit
 
-class SelectBankViewController: UIViewController {
+import SnapKit
+import Then
 
+final class SelectBankViewController: UIViewController {
+
+    // MARK: - UI Properties
+    
+    private let cancelButton = UIButton()
+    
+    private let selectBankHeader = SelectBankHeaderView()
+    
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Life Cycles
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setHierarchy()
+        setLayout()
+        setStyle()
+    }
+
+
+}
+
+// MARK: - Private Methods
+
+private extension SelectBankViewController {
+    
+    func setHierarchy() {
+        self.view.addSubviews(cancelButton, selectBankHeader)
+        self.view.bringSubviewToFront(cancelButton)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setLayout() {
+        cancelButton.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(11)
+            $0.trailing.equalToSuperview().inset(9)
+            $0.size.equalTo(44)
+        }
+        
+        selectBankHeader.snp.makeConstraints {
+            $0.top.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(204)
+        }
     }
-    */
-
+    
+    func setStyle() {
+        self.view.backgroundColor = UIColor(resource: .white)
+        self.navigationController?.navigationBar.isHidden = true
+        
+        cancelButton.do {
+            $0.backgroundColor = UIColor(resource: .white)
+            $0.setTitleColor(UIColor(resource: .black2), for: .normal)
+            $0.setTitleColor(UIColor(resource: .black2), for: .highlighted)
+            $0.setAttributedTitle(UILabel.attributedText(for: .body2, withText: "취소"), for: .normal)
+        }
+        
+        selectBankHeader.do {
+            $0.segmentView.addTarget(self, action: #selector(didChangeValue(sender: )), for: .valueChanged)
+        }
+    }
+    
+    @objc
+    func didChangeValue(sender: UISegmentedControl) {
+        print("change to \(sender.selectedSegmentIndex)")
+    }
 }
--- KAKAOBANK-iOS/KAKAOBANK-iOS/Presentation/SelectBank/Views/SegmentView.swift
+++ KAKAOBANK-iOS/KAKAOBANK-iOS/Presentation/SelectBank/Views/SegmentView.swift
@@ -7,14 +7,74 @@
 
 import UIKit
 
-class SegmentView: UISegmentedControl {
+import SnapKit
+import Then
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class SegmentView: UISegmentedControl {
+
+    // MARK: - UI Properties
+    
+    private let underline = UIView()
+    
+    
+    // MARK: - Properties
+    
+
+    
+    // MARK: - UI Properties
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
+    
+    override init(items: [Any]?) {
+        super.init(items: items)
+        
+        setHierarchy()
+        removeBackgroundAndDivider()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        setUnderline()
+    }
+    
+    func setUnderline() {
+        underline.backgroundColor = UIColor(resource: .gray7)
+        
+        let width = self.widthForSegment(at: self.selectedSegmentIndex)
+        let height = CGFloat(3)
 
+        let xPosition = width * CGFloat(self.selectedSegmentIndex)
+        let yPosition = self.frame.height - height
+        
+        UIView.animate(withDuration: 0.1) {
+            self.underline.frame = CGRect(x: xPosition, y: yPosition, width: width, height: height)
+        }
+        
+    }
+}
+
+
+// MARK: - UI Properties
+
+private extension SegmentView {
+    
+    func setHierarchy() {
+        self.addSubview(underline)
+    }
+    
+    func removeBackgroundAndDivider() {
+        let image = UIImage()
+        self.setBackgroundImage(image, for: .normal, barMetrics: .default)
+        self.setBackgroundImage(image, for: .selected, barMetrics: .default)
+        self.setBackgroundImage(image, for: .highlighted, barMetrics: .default)
+        self.setDividerImage(image, forLeftSegmentState: .selected, rightSegmentState: .normal, barMetrics: .default)
+    }
+    
 }
