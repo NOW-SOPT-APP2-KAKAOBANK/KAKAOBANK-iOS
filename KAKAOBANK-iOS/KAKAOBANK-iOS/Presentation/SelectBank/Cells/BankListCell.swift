e7cfc0809d281cd89bbeeb5b5c79cfc5cedb539e
seuriseuljjeok <heeseul09@naver.com>
2024-05-16 15:19:14 +0900

    [Add] 은행, 증권 더미데이터 추가 (#18)

---
+++ KAKAOBANK-iOS/KAKAOBANK-iOS/Presentation/SelectBank/Cells/BankListCell.swift
@@ -0,0 +1,12 @@
+//
+//  BankListCell.swift
+//  KAKAOBANK-iOS
+//
+//  Created by 윤희슬 on 5/16/24.
+//
+
+import UIKit
+
+class BankListCell: UICollectionViewCell {
+    
+}
---
+++ KAKAOBANK-iOS/KAKAOBANK-iOS/Presentation/SelectBank/Views/DimmedView.swift
@@ -0,0 +1,20 @@
+//
+//  DimmedView.swift
+//  KAKAOBANK-iOS
+//
+//  Created by 윤희슬 on 5/16/24.
+//
+
+import UIKit
+
+class DimmedView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
