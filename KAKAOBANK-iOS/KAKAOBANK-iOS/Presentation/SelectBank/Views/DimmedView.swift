316763079ba08b4c6c0df22bbb68e4e03549aae7
seuriseuljjeok <heeseul09@naver.com>
2024-05-16 15:20:41 +0900

    [Feat] 바텀시트 뒤 배경뷰 구현 (#18)

--- KAKAOBANK-iOS/KAKAOBANK-iOS/Presentation/SelectBank/Views/DimmedView.swift
+++ KAKAOBANK-iOS/KAKAOBANK-iOS/Presentation/SelectBank/Views/DimmedView.swift
@@ -7,14 +7,35 @@
 
 import UIKit
 
-class DimmedView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class DimmedView: UIView {
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
 
+
+// MARK: - Private Methods
+
+private extension DimmedView {
+    
+    func setHierarchy() {
+        
+    }
+    
+    func setLayout() {
+        
+    }
+    
+    func setStyle() {
+        
+    }
+    
 }
