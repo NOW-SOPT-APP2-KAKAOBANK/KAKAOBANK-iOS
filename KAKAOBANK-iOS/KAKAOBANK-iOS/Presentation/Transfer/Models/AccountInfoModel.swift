6df80c16c2cfb091af0acd811f1a2b0384c670dc
seuriseuljjeok <heeseul09@naver.com>
2024-05-21 19:13:05 +0900

    [Setting] 이체 관련 데이터 모델 수정 (#50)

--- KAKAOBANK-iOS/KAKAOBANK-iOS/Presentation/Transfer/Models/AccountInfoModel.swift
+++ KAKAOBANK-iOS/KAKAOBANK-iOS/Presentation/Transfer/Models/AccountInfoModel.swift
@@ -8,21 +8,27 @@
 import UIKit
 
 struct AccountInfoModel {
-    let bankImg: UIImage
-    let bankbookName: String
-    let accountNumber: String
-    let favorites: Bool? = false
+    let accountName: String
+    let accountNumber: Int
+    let isAccountLike: Bool
+    let bankName: String
+    let imgURL: String
+    let accountID: Int
 }
 
 extension AccountInfoModel {
     
-    static let myAccountInfoAppData:  [AccountInfoModel] = [
-        AccountInfoModel(bankImg: .btnKakaoBankIos, bankbookName: "윤희슬의 통장", accountNumber: "카카오뱅크 3333-23-9165754")
-    ]
+//    static let myAccountInfoAppData:  [AccountInfoModel] = [
+//        AccountInfoModel(bankImg: .btnKakaoBankIos, bankbookName: "윤희슬의 통장", accountNumber: "카카오뱅크 3333-23-9165754")
+//    ]
+//    
+//    static let recentTransferInfoAppData:  [AccountInfoModel] = [
+//        AccountInfoModel(bankImg: .btnNonghupBankIos, bankbookName: "윤희슬", accountNumber: "농협 3333-23-9165754"),
+//        AccountInfoModel(bankImg: .btnKakaoBankIos, bankbookName: "김민서", accountNumber: "카카오 뱅크 3333-23-9165754"),
+//        AccountInfoModel(bankImg: .btnKukminBankIos, bankbookName: "오서영", accountNumber: "국민 3333-23-9165754")
+//    ]
     
-    static let recentTransferInfoAppData:  [AccountInfoModel] = [
-        AccountInfoModel(bankImg: .btnNonghupBankIos, bankbookName: "윤희슬", accountNumber: "농협 3333-23-9165754"),
-        AccountInfoModel(bankImg: .btnKakaoBankIos, bankbookName: "김민서", accountNumber: "카카오 뱅크 3333-23-9165754"),
-        AccountInfoModel(bankImg: .btnKukminBankIos, bankbookName: "오서영", accountNumber: "국민 3333-23-9165754")
-    ]
+        static let myAccountInfoAppData:  [AccountInfoModel] = [
+            AccountInfoModel(accountName: "윤희슬의 통장", accountNumber: 3333239165754, isAccountLike: false, bankName: "카카오뱅크", imgURL: "",  accountID: 1)
+        ]
 }
