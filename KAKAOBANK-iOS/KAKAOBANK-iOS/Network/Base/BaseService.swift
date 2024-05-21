1612937641af08c332dc3a507bf1d4192b68fd15
seuriseuljjeok <heeseul09@naver.com>
2024-05-21 16:32:56 +0900

    [Setting] 서버 통신 기본 세팅 (#50)

---
+++ KAKAOBANK-iOS/KAKAOBANK-iOS/Network/Base/BaseService.swift
@@ -0,0 +1,34 @@
+//
+//  BaseService.swift
+//  KAKAOBANK-iOS
+//
+//  Created by 윤희슬 on 5/21/24.
+//
+
+import Foundation
+
+class BaseService {
+    
+    func judgeStatus<T: Codable>(statusCode: Int, data: Data, _ responseType: T.Type) -> NetworkResult<Any> {
+        switch statusCode {
+        case 200..<205:
+            return isValidData(data: data, T.self)
+        case 400..<500:
+            return .requestErr
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+    
+    func isValidData<T: Codable>(data: Data, _ responseType: T.Type) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(T.self, from: data) else {
+            print("⛔️ \(self)애서 디코딩 오류가 발생했습니다 ⛔️")
+            return .pathErr
+        }
+        
+        return .success(decodedData as Any)
+    }
+}
